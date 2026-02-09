bff67d56a4dbf4ee89b830dacf2574f08fb5e132
sheidyramirez <sheidyyilmara246@gmail.com>
2026-02-09 13:44:03 -0500

    Add files via upload

---
+++ Corte1/Practica2/Calculator.java
@@ -0,0 +1,32 @@
+package calculator;
+
+public class Calculator {
+    public int x1, x2;
+
+//Constructor
+    public Calculator (int a, int b){
+        x1 = a;
+        x2 = b;
+    }
+    public int add(){
+        return x1 + x2;
+}
+    
+    public int subs(){
+        return x1 - x2;
+}
+    public int multiplication(){
+        return x1 * x2;
+}
+    public int div(){
+        return x1 / x2;
+}
+    
+    public static void main(String[] args) {
+        Calculator m = new Calculator(34, 43);
+        System.out.println(m.add());
+        System.out.println(m.subs());
+        System.out.println(m.multiplication());
+        System.out.println(m.div());
+    }
+}  
