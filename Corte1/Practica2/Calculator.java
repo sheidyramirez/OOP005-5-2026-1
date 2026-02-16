37b98203f0faa93b7b72d2c5438d09b61bf1f6c4
sheidyramirez <sheidyyilmara246@gmail.com>
2026-02-16 12:37:47 -0500

    Update Calculator.java

--- Corte1/Practica2/Calculator.java
+++ Corte1/Practica2/Calculator.java
@@ -19,21 +19,28 @@ public class Calculator {
         return x1 * x2;
 }
     public int div(){
-       if (x2 == 0) {
+        if (x2==0){
             System.out.println("No se puede dividir entre cero");
             return 0;
-        } else {
-            return x1 / x2;
+        } else{
+            return x1/x2;
         }
-    }
+}
     
     public static void main(String[] args) {
         Calculator m = new Calculator(34, 43);
-        System.out.println(m.add());
-        System.out.println(m.subs());
-        System.out.println(m.multiplication());
-        System.out.println(m.div());
+        System.out.println("La suma m es:" + m.add());
+        System.out.println("La resta m es:" + m.subs());
+        System.out.println("La multiplicacion m es:" + m.multiplication());
+        System.out.println("La division m es:" + m.div());
+        
+        Calculator n = new Calculator(15, 0);
+        System.out.println("La suma n es:" + n.add());
+        System.out.println("La resta n es:" + m.subs());
+        System.out.println("La multiplicacion n es:" + m.multiplication());
+        System.out.println("La division n es:" + m.div());
     }
-}  
+} 
+
 
 
