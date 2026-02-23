a68628b9e128f38f2fc3a2e67b9701c48a640dbb
sheidyramirez <sheidyyilmara246@gmail.com>
2026-02-23 13:20:34 -0500

    Update TreinandoArrays1.java

--- Corte1/Practica3/TreinandoArrays1.java
+++ Corte1/Practica3/TreinandoArrays1.java
@@ -21,7 +21,40 @@ public class TreinandoArrays1 {
         }
         System.out.println(" ] ") ;
     }
-
+    
+    //Metodo suma
+    public int sum (){
+        int s = 0;
+        for (int i = 0; i < n; i++) s += data[i];
+        return s;
+    }
+    
+    //Metodo promedio
+    public double mean (){
+        if (n == 0) return 0.0;
+        return (double) sum() / (double) n;
+    }
+    
+    //Metodo maximo
+    public int max () {
+        int m = data [0];
+        for (int i = 1; i < n; i++){
+            if (data[i] > m) 
+                m = data[i];
+        }
+        return m;
+    }
+    
+    //Metodo min
+    public int min () {
+        int m = data [0];
+        for (int i = 1; i < n; i++){
+        if (data[i] < m) 
+            m = data[i];
+        }
+        return m;
+    }
+    
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -34,13 +67,12 @@ public class TreinandoArrays1 {
             ob1.data[i] = sc.nextInt();
         }
 
-        ob1.print("Hola"); 
-
-        System.out.println(ob1.data[0]);
-        System.out.println(ob1.data[1]);
-        System.out.println(ob1.data[2]);
-        System.out.println(ob1.data[3]);
-
+        ob1.print("Vector"); 
+        
+        System.out.println("La suma de los elementos del vector: " + ob1.sum());
+        System.out.println("El promedio de los elementos del vector: " + ob1.mean());
+        System.out.println("El max de los elementos del vector: " + ob1.max());
+        System.out.println("El min de los elementos del vector: " + ob1.min());
         sc.close();
     }
 }
