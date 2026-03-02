e2e4c81eb6a96404ad34848df253cff5abb4bfe9
sheidyramirez <sheidyyilmara246@gmail.com>
2026-03-02 13:40:51 -0500

    Add files via upload

---
+++ Corte1/Practica4/Matriz2x2.java
@@ -0,0 +1,44 @@
+package matriz2x2;
+
+import java.util.Scanner;
+
+public class Matriz2x2 {
+
+    public int[][] data;
+    public int filas;
+    public int columnas;
+
+    public Matriz2x2() {
+        filas = 2;
+        columnas = 2;
+        data = new int[filas][columnas];
+    }
+
+    public void set() {
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                System.out.print("data[" + i + "][" + j + "]: ");
+                data[i][j] = sc.nextInt();
+            }
+        }
+    }
+
+    public void get() {
+        System.out.println("\nMatriz 2x2:");
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                System.out.print(data[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Matriz2x2 m = new Matriz2x2();
+        m.set();
+        m.get();
+    }
+} 
+
