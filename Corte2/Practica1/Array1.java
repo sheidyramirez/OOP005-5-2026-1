3bc48b3e1e0a33e763587bcf691dadabff3c2570
sheidyramirez <sheidyyilmara246@gmail.com>
2026-04-13 12:55:36 -0500

    Refactor Array1 to TreinandoArrays1 with additional methods
    The Array1 class has been replaced with TreinandoArrays1, which includes methods for sum, mean, max, and min calculations.

--- Corte2/Practica1/Array1.java
+++ Corte2/Practica1/Array1.java
@@ -1,54 +1,78 @@
-package arrays1;
+package treinandoarrays1;
 
 import java.util.Scanner;
 
-public class Array1 
-{
-    public int[] data;
-    public int n;
+public class TreinandoArrays1 {
 
-    public Array1(int n) 
-    {
-        this.n = n;
-        this.data = new int[n];   // Crear el arreglo
+    public int [] data ;
+    public int n ;
+
+    // Constructor : crea vector de tamaño n
+    public TreinandoArrays1( int n ) {
+       this . n = n ;
+       this . data = new int[n];
     }
 
-    public void getvector(String name) 
-    {
-        System.out.print(name + " = [ ");
-        for (int i = 0; i < n ; i++) 
-        {
-            System.out.print(data[i]);
-            if (i < n - 1) 
-            {
-                System.out.print(" , ");
-            }
+    public void print( String name ) {
+        System.out.print(name + " = [ " ) ;
+        for (int i = 0; i < n ; i ++) {
+           System . out . print ( data [ i ]) ;
+           if(i < n - 1) System . out . print ( " , " ) ;
         }
-        System.out.println(" ]");
-    }
-
-    public void setvector(String name)
-    {
-        Scanner sc = new Scanner(System.in);
-        
-        for (int i = 0; i < n ; i++) 
-        {
-            System.out.print("v[" + i + "]: ");
-            data[i] = sc.nextInt();
-        }
+        System.out.println(" ] ") ;
     }
     
-    public static void main(String[] args) 
-    {
+    //Metodo suma
+    public int sum (){
+        int s = 0;
+        for (int i = 0; i < n; i++) s += data[i];
+        return s;
+    }
+    
+    //Metodo promedio
+    public double mean (){
+        if (n == 0) return 0.0;
+        return (double) sum() / (double) n;
+    }
+    
+    //Metodo maximo
+    public int max () {
+        int m = data [0];
+        for (int i = 1; i < n; i++){
+            if (data[i] > m) 
+                m = data[i];
+        }
+        return m;
+    }
+    
+    //Metodo min
+    public int min () {
+        int m = data [0];
+        for (int i = 1; i < n; i++){
+        if (data[i] < m) 
+            m = data[i];
+        }
+        return m;
+    }
+    
+    public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Tamano n: ");
-        int n = sc.nextInt();
+        TreinandoArrays1 ob1 = new TreinandoArrays1(8);
 
-        Array1 v = new Array1(n);
-        v.setvector("Vector");
-        v.getvector("Vector");
+        // Ciclo para llenar el vector
+        for (int i = 0; i < ob1.n; i++) {
+            System.out.print("Ingrese el numero para la posicion " + i + ": ");
+            ob1.data[i] = sc.nextInt();
+        }
 
+        ob1.print("Vector"); 
+        
+        System.out.println("La suma de los elementos del vector: " + ob1.sum());
+        System.out.println("El promedio de los elementos del vector: " + ob1.mean());
+        System.out.println("El max de los elementos del vector: " + ob1.max());
+        System.out.println("El min de los elementos del vector: " + ob1.min());
         sc.close();
     }
 }
