ed17cb0134d35b65b864663286135f8d47ce5685
sheidyramirez <sheidyyilmara246@gmail.com>
2026-04-19 22:35:17 -0500

    Update account names in Bank class

--- Corte2/Practica5/Bank.java
+++ Corte2/Practica5/Bank.java
@@ -10,10 +10,10 @@ public class Bank{
   public Bank(){
     accountOne = new CheckingAccount("Zeus", 100, "1");
     accountTwo = new CheckingAccount("Hades", 200, "2");
-    accountThree = new CheckingAccount("Lucas", 300, "3");
-    accountFour = new CheckingAccount("Maria", 400, "4");
-    accountFive = new CheckingAccount("Angela", 500, "5");
-    accountSix = new CheckingAccount("Luis", 600, "6");
+    accountThree = new CheckingAccount("Afrodita", 300, "3");
+    accountFour = new CheckingAccount("Atenea", 400, "4");
+    accountFive = new CheckingAccount("Cronos", 500, "5");
+    accountSix = new CheckingAccount("Poseidon", 600, "6");
   }
 
   public static void main(String[] args){
